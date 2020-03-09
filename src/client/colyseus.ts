11e8cf052dea0d9fb4ef76fc62873a558a3aa741
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-03-09 22:52:38 +0100

    try wss

--- src/client/colyseus.ts
+++ src/client/colyseus.ts
@@ -2,6 +2,7 @@ import * as Colyseus from "colyseus.js";
 
 const hostname = location.hostname || "localhost";
 const port = hostname === "localhost" ? 8080 : 80;
-const client = new Colyseus.Client(`ws://${hostname}:${port}`);
+const protocol = hostname === "localhost" ? "ws" : "wss";
+const client = new Colyseus.Client(`${protocol}://${hostname}:${port}`);
 
 export default client;
