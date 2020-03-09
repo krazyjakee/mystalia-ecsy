bfafb1915e008afa009a2c24893e8e674eecc916
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-03-09 22:57:41 +0100

    ssl port

--- src/client/colyseus.ts
+++ src/client/colyseus.ts
@@ -1,7 +1,7 @@
 import * as Colyseus from "colyseus.js";
 
 const hostname = location.hostname || "localhost";
-const port = hostname === "localhost" ? 8080 : 80;
+const port = hostname === "localhost" ? 8080 : 443;
 const protocol = hostname === "localhost" ? "ws" : "wss";
 const client = new Colyseus.Client(`${protocol}://${hostname}:${port}`);
 
