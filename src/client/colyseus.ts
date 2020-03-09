4dc4573ccb68b36d6e6c4988edac410ff2f17039
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-03-09 21:13:10 +0100

    colyseus should start on env port

--- src/client/colyseus.ts
+++ src/client/colyseus.ts
@@ -1,5 +1,7 @@
 import * as Colyseus from "colyseus.js";
 
-const client = new Colyseus.Client("ws://localhost:8080");
+const client = new Colyseus.Client(
+  `ws://localhost:${process.env.PORT || 8080}`
+);
 
 export default client;
