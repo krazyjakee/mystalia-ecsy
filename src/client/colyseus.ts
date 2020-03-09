4be02d05a5654407fb6461d54d275e21470437f3
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-03-09 22:36:40 +0100

    fix colyseus url

--- src/client/colyseus.ts
+++ src/client/colyseus.ts
@@ -1,7 +1,7 @@
 import * as Colyseus from "colyseus.js";
 
-const client = new Colyseus.Client(
-  `ws://localhost:${process.env.PORT || 8080}`
-);
+const hostname = location.hostname || "localhost";
+const port = hostname === "localhost" ? 8080 : 80;
+const client = new Colyseus.Client(`ws://${hostname}:${port}`);
 
 export default client;
