419b773037ecf0261086a2ef486e731e20fcd4af
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-04-28 23:41:20 +0200

    surroundings logic

---
+++ src/utilities/movement/surroundings.test.ts
@@ -0,0 +1,56 @@
+import { ObjectTileStore } from "utilities/ObjectTileStore";
+import { readMapFiles } from "@server/utilities/mapFiles";
+import { tilesInRadiusOf, distanceBetweenTiles } from "./surroundings";
+
+const createObjectTileStore = () => {
+  const maps = readMapFiles();
+  const data = maps["test"];
+  const ots = new ObjectTileStore(data);
+  return ots;
+};
+
+describe("surroundings", () => {
+  const ots = createObjectTileStore();
+  const size = { width: ots.columns, height: ots.rows };
+
+  describe("#distanceBetweenTiles", () => {
+    test("correctly calculate distance", () => {
+      let tiles = distanceBetweenTiles(2, 22, size.width);
+      expect(tiles).toEqual(2);
+
+      tiles = distanceBetweenTiles(22, 33, size.width);
+      expect(tiles).toEqual(1);
+
+      tiles = distanceBetweenTiles(0, 22, size.width);
+      expect(tiles).toEqual(2);
+    });
+  });
+
+  describe("#tilesInRadiusOf", () => {
+    test("correctly list valid surrounding tiles with radius 1", () => {
+      const tiles = tilesInRadiusOf(25, size);
+      expect(tiles).toStrictEqual([14, 15, 16, 24, 26, 34, 35, 36]);
+    });
+
+    test("correctly list valid surrounding tiles at Y edge", () => {
+      const tilesTop = tilesInRadiusOf(4, size);
+      expect(tilesTop).toStrictEqual([3, 5, 13, 14, 15]);
+
+      const tilesBottom = tilesInRadiusOf(94, size);
+      expect(tilesBottom).toStrictEqual([83, 84, 85, 93, 95]);
+    });
+
+    test("correctly list valid surrounding tiles at X edge", () => {
+      const tilesLeft = tilesInRadiusOf(30, size);
+      expect(tilesLeft).toStrictEqual([20, 21, 31, 40, 41]);
+
+      const tilesRight = tilesInRadiusOf(39, size);
+      expect(tilesRight).toStrictEqual([28, 29, 38, 48, 49]);
+    });
+
+    test("correctly list valid surrounding tiles with radius 2", () => {
+      const tiles = tilesInRadiusOf(0, size, 2);
+      expect(tiles).toStrictEqual([1, 2, 10, 11, 12, 20, 21, 22]);
+    });
+  });
+});
---
+++ src/utilities/movement/surroundings.ts
@@ -0,0 +1,69 @@
+import { AStarFinder } from "astar-typescript";
+import { Size } from "types/TileMap/standard";
+import { isPresent } from "utilities/guards";
+
+export const tilesInRadiusOf = (
+  tileId: number,
+  mapSize: Size,
+  radius: number = 1
+) => {
+  const columns = mapSize.width;
+  const rows = mapSize.height;
+  const radiusWidth = radius * 2 + 1;
+  const totalTiles = radiusWidth * radiusWidth;
+  const firstTile = tileId - columns * radius - radius;
+
+  const tiles: number[] = [];
+  let count = 0;
+  let row = 0;
+  let column = 0;
+
+  while (count < totalTiles) {
+    tiles.push(firstTile + column + row * columns);
+    column += 1;
+    if (column === radiusWidth) {
+      column = 0;
+      row += 1;
+    }
+    count += 1;
+  }
+  return tiles.filter((tile) => {
+    if (tile === tileId || tile < 0 || tile > columns * rows - 1) {
+      return false;
+    }
+
+    const distance = distanceBetweenTiles(tileId, tile, columns);
+    return distance <= radius;
+  });
+};
+
+export const distanceBetweenTiles = (
+  from: number,
+  to: number,
+  mapColumns: number
+) => {
+  const fromX = from % mapColumns;
+  const toX = to % mapColumns;
+  const maxX = Math.max(fromX, toX);
+  const minX = Math.min(fromX, toX);
+  const x = maxX - minX;
+
+  const fromY = from / mapColumns;
+  const toY = to / mapColumns;
+  const maxY = Math.max(fromY, toY);
+  const minY = Math.min(fromY, toY);
+  const y = Math.round(maxY - minY);
+
+  let result = x + y;
+  if (x && y) {
+    result -= 1;
+  }
+  // TODO fix failing tests
+  return result;
+};
+
+export const shortestPathTo = (
+  aStar: AStarFinder,
+  from: number,
+  to: number
+) => {};
