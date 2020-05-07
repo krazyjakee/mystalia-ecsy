cdbfb060ef6469a25b526287c8b85b88685a5e57
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-05-07 21:59:25 +0200

    fix for #60

--- src/client/systems/Rendering/GraySystem.ts
+++ src/client/systems/Rendering/GraySystem.ts
@@ -4,6 +4,7 @@ import { Gray } from "@client/components/Tags";
 import context2d from "../../canvas";
 import NetworkRoom from "@client/components/NetworkRoom";
 import gameState from "../../gameState";
+import Weather from "@client/components/Weather";
 
 let grayScale = 0;
 
@@ -23,6 +24,8 @@ export default class GraySystem extends System {
 
   execute() {
     this.queries.loadedTileMaps.results.forEach((tileMapEntity) => {
+      tileMapEntity.removeComponent(Weather);
+
       if (grayScale <= 100) {
         context2d.filter = `grayscale(${grayScale}%)`;
         grayScale += 1;
