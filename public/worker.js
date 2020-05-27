77ba7ec9433daf31f74f914cd8431ffb331ba249
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2020-05-27 23:27:55 +0200

    correctly retire worker

--- public/worker.js
+++ public/worker.js
@@ -1,3 +1,17 @@
-setInterval(function() {
-  postMessage("");
-}, 1000 / 50);
+var timer;
+
+var startTimer = () => {
+  timer = setInterval(function() {
+    postMessage("");
+  }, 1000 / 50);
+};
+
+onmessage = function(e) {
+  if (e.data === "start") {
+    startTimer();
+  }
+  if (e.data === "stop") {
+    clearInterval(timer);
+    postMessage("stopped");
+  }
+};
