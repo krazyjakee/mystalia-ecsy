884dd0291c73a3dcea6c414c9eda9a77b430d50f
Josh <josh@artificialorigins.com>
2020-08-11 21:51:42 +0100

    color functions tidy

--- src/utilities/color.ts
+++ src/utilities/color.ts
@@ -1,21 +1,28 @@
 import memoize from "./memoize";
 
-export const hexToRgb = memoize((hex: string, opacity: number) => {
-  var hasHashReg = /^#/
-  var regexToCheckInput = /^#([0-9A-F]{3}){1,2}$/i
+export const hexToRgb = memoize((hex: string, opacity: number, returnObj?: boolean) : any => {
+  const hasHashReg = /^#/
+  const regexToCheckInput = /^#([0-9A-F]{3}){1,2}$/i
 
   if (!hasHashReg.test(hex)) {
-    hex = '#'+hex
+    hex = "#"+hex
   }
   if (hex.length > 7) {
     hex = hex.substring(0, hex.length - (hex.length - 7))
   }
   if (regexToCheckInput.test(hex)) { //is a valid hex value
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, (m, r, g, b) => {
       return r + r + g + g + b + b;
     });
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    if (returnObj === true) {
+      return result
+      ? {"r": parseInt(result[1], 16),"g": parseInt(result[2], 16),"b": parseInt(result[3], 16), "a": opacity}
+      : {"r": 255,"g": 255,"b": 255, "a": opacity};
+    }
+
     return result
       ? `rgba(${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(
           result[3],
@@ -26,32 +33,22 @@ export const hexToRgb = memoize((hex: string, opacity: number) => {
   return `rgba(255,255,255,${opacity})`;
 });
 
-export const hexToRgbObj = memoize((hex: string, opacity: number = 1) : { r: number; g: number; b: number; a?: number; } => {
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-    return r + r + g + g + b + b;
-  });
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {'r': parseInt(result[1], 16),'g': parseInt(result[2], 16),'b': parseInt(result[3], 16), 'a': opacity}
-    : {'r': 255,'g': 255,'b': 255, 'a': opacity};
-});
-
 export const hexToHslObj = memoize((hex: string) : { h: number; s: number; l: number; } => {
-  var rgbObj = hexToRgbObj(hex);
-  var r = rgbObj.r
-  var g = rgbObj.g
-  var b = rgbObj.b
+  const rgbObj = hexToRgb(hex, 1, true);
+  let r = rgbObj.r
+  let g = rgbObj.g
+  let b = rgbObj.b
 
   r /= 255, g /= 255, b /= 255;
 
-  var max = Math.max(r, g, b), min = Math.min(r, g, b);
-  var h, s, l = (max + min) / 2;
+  const max = Math.max(r, g, b) 
+  const min = Math.min(r, g, b)
+  let h, s, l = (max + min) / 2;
 
-  if (max == min) {
+  if (max === min) {
     h = s = 0; // achromatic
   } else {
-    var d = max - min;
+    const d = max - min;
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
 
     switch (max) {
@@ -66,12 +63,14 @@ export const hexToHslObj = memoize((hex: string) : { h: number; s: number; l: nu
 });
 
 export const hslObjToRgbObj = memoize((hsl: { h: number; s: number; l: number; }) : { r: number; g: number; b: number; } => {
-  var r, g, b;
-  var h = hsl.h / 360
-  var s = hsl.s / 100
-  var l = hsl.l / 100
+  let r
+  let g
+  let b
+  const h = hsl.h / 360
+  const s = hsl.s / 100
+  const l = hsl.l / 100
 
-  if (s == 0) {
+  if (s === 0) {
     r = g = b = l; // achromatic
   } else {
     function hue2rgb(p, q, t) {
@@ -83,8 +82,8 @@ export const hslObjToRgbObj = memoize((hsl: { h: number; s: number; l: number; }
       return p;
     }
 
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
 
     r = hue2rgb(p, q, h + 1/3);
     g = hue2rgb(p, q, h);
